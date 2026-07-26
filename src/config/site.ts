8148bf0339629f67ffbf9fb600dd819f00d9e206
Fadul Saifeldin MohamedAhmed Osman <141461068+FadulSaif@users.noreply.github.com>
2026-07-26 18:37:48 +0800

    Delete src directory

--- src/config/site.ts
+++
@@ -1,3 +0,0 @@
-export const SITE_URL = (import.meta.env.VITE_SITE_URL || 'https://rec-ebon.vercel.app').replace(/\/$/, '');
-
-export const toAbsoluteUrl = (pathname: string) => `${SITE_URL}${pathname.startsWith('/') ? pathname : `/${pathname}`}`;
